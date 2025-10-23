cba6a2e8374df4f03274d3ace05b5c9613dae927
KitKat0098 <catherine.sijo@marcellin.school.nz>
2025-10-24 10:57:08 +1300

    Completed sprint one and added a start button

--- dgt-informative-game/scripts/game_manager.gd
+++ dgt-informative-game/scripts/game_manager.gd
@@ -6,7 +6,4 @@ var score = 0
 
 func add_point():
 	score += 1
-	if score == 1:
-		score_label.text = "You collected " + str(score) + " coin."
-	else:
-		score_label.text = "You collected " + str(score) + " coins."
+	score_label.text = "You collected " + str(score) + " out of 9 coins."
